8560d193489d57ade1cf67d3d41a9491934d1421
marvinstanley <48851139+marvinstanley@users.noreply.github.com>
2019-04-15 01:20:05 +0700

    Update pre-processing-tweets.py
    Added the ability read and write .txt tweets

--- pre-processing-tweets.py
+++ pre-processing-tweets.py
@@ -6,19 +6,28 @@ import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
-sentence = "RT @Gemacan70: Ayoo jaga keutuhan NKRI!!! Tangkap Penista Agama!!!  https://t.co/dxIDQ4ZQHV"
+#VARIABLES
+inputPath = "data/labeled/2019-04-15.txt"
+outputPath = "data/preprocessed/2019-04-15.txt"
 
-norm = Normalizer("lib/formalizationDict.txt")
-
-# removes RT from tweets
-if sentence[:3] == "RT ":
-    sentence = sentence[3:]
-
-sentence = norm.normalize(sentence)
-
-word_tokens = word_tokenize(sentence)
-indonesian_stops = stopwords.words('indonesian')
-no_stops = [words for words in word_tokens if words not in indonesian_stops]
-
-print(no_stops)
+#ALGORITHM
+if os.path.isfile(inputPath):
+    norm = Normalizer("lib/formalizationDict.txt")
+    txtFile = open(inputPath,"r")
+    output = open(outputPath,"w")
+    for i,j in enumerate(txtFile):
+        label = j[:1]
+        sentence = (j[2:len(j)-1])
+        if sentence[:3] == "RT ": # removes RT from tweets
+            sentence = sentence[3:]
+        sentence = norm.normalize(sentence)
+        word_tokens = word_tokenize(sentence)
+        indonesian_stops = stopwords.words('indonesian')
+        no_stops = [words for words in word_tokens if words not in indonesian_stops]
+        toWrite = label+'\t'+str(no_stops)+'\n'
+        output.write(toWrite)
+        print(no_stops)
+    txtFile.close()
+else:
+    print('FILE NOT FOUND')
 
